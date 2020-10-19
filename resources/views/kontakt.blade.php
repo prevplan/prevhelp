4884dae63bba23232f0c6e160057ef41799a4e51
Holger Schmermbeck <hosch@mailbox.org>
2020-10-19 10:04:03 +0200

    clean design

--- resources/views/kontakt.blade.php
+++ resources/views/kontakt.blade.php
@@ -56,94 +56,24 @@
                     <input type="hidden" value="true" name="emailSent" id="emailSent">
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label class="required font-weight-bold text-dark text-2">Full Name</label>
+                            <label class="font-weight-bold text-dark text-2">Name</label>
                             <input type="text" value="" data-msg-required="Please enter your name." maxlength="100" class="form-control" name="name" id="name" required>
                         </div>
                         <div class="form-group col-md-6">
-                            <label class="required font-weight-bold text-dark text-2">Email Address</label>
+                            <label class="font-weight-bold text-dark text-2">E-Mail Addresse</label>
                             <input type="email" value="" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." maxlength="100" class="form-control" name="email" id="email" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-12">
-                            <label class="font-weight-bold text-dark text-2">Subject</label>
-                            <select data-msg-required="Please enter the subject." class="form-control" name="subject" id="subject" required>
-                                <option value="">...</option>
-                                <option value="Option 1">Option 1</option>
-                                <option value="Option 2">Option 2</option>
-                                <option value="Option 3">Option 3</option>
-                                <option value="Option 4">Option 4</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <p class="mb-2">Checkboxes</p>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" name="checkboxes[]" type="checkbox" data-msg-required="Please select at least one option." id="inlineCheckbox1" value="option1"> 1
-                                </label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" name="checkboxes[]" type="checkbox" data-msg-required="Please select at least one option." id="inlineCheckbox1" value="option2"> 2
-                                </label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" name="checkboxes[]" type="checkbox" data-msg-required="Please select at least one option." id="inlineCheckbox1" value="option3"> 3
-                                </label>
-                            </div>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <p class="mb-2">Radios</p>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="radios" data-msg-required="Please select at least one option." id="inlineRadio1" value="option1"> 1
-                                </label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="radios" data-msg-required="Please select at least one option." id="inlineRadio2" value="option2"> 2
-                                </label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="radio" name="radios" data-msg-required="Please select at least one option." id="inlineRadio3" value="option3"> 3
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-12">
-                            <label class="font-weight-bold text-dark text-2">Attachment</label>
-                            <input class="d-block" type="file" name="attachment" id="attachment">
+                            <label class="font-weight-bold text-dark text-2">Betreff</label>
+                            <input type="text" value="" data-msg-required="Please enter your name." maxlength="100" class="form-control" name="subject" id="subject" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-12 mb-4">
-                            <label class="required font-weight-bold text-dark text-2">Message</label>
+                            <label class="font-weight-bold text-dark text-2">Ihre Nachricht an uns</label>
                             <textarea maxlength="5000" data-msg-required="Please enter your message." rows="6" class="form-control" name="message" id="message" required></textarea>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-12 mb-0">
-                            <label class="font-weight-bold text-dark text-2">Human Verification</label>
-                        </div>
-                    </div>
-                    <div class="form-row pt-2">
-                        <div class="form-group col-md-4">
-                            <div class="captcha form-control">
-                                <div class="captcha-image">
-
-                                </div>
-                                <div class="captcha-refresh">
-                                    <a href="#" id="refreshCaptcha"><i class="fas fa-sync"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group col-md-8">
-                            <input type="text" value="" maxlength="6" data-msg-captcha="Wrong verification code." data-msg-required="Please enter the verification code." placeholder="Type the verification code." class="form-control form-control-lg captcha-input" name="captcha" id="captcha" required>
                         </div>
                     </div>
                     <div class="form-row">
@@ -153,7 +83,7 @@
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-12 mb-5">
-                            <input type="submit" id="contactFormSubmit" value="Send Message" class="btn btn-primary btn-modern pull-right" data-loading-text="Loading...">
+                            <input type="submit" id="contactFormSubmit" value="Nachricht absenden" class="btn btn-primary btn-modern pull-right" data-loading-text="Loading...">
                         </div>
                     </div>
                 </form>
