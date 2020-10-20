be6a04237a29c1209ad23e2100298ea3cb0a4337
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2020-10-20 12:21:34 +0200

    added spatie/laravel-sitemap  (#11)
    * added  spatie/laravel-sitemap
    
    * added artisan sitemap command
    
    * added sitemap.xml to .gitignore

---
+++ app/Console/Commands/sitemap.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
+
+class sitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $path = public_path('sitemap.xml');
+        SitemapGenerator::create(config('app.url'))
+          /*  ->getSitemap()
+            ->add(Url::create('/besondere-seite')
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(0.5)) */
+            ->writeToFile($path);
+    }
+}
