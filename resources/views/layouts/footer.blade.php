2f2230f9664c15ac8c6242ccb8cfb415ce1c1825
Holger Schmermbeck <hosch@mailbox.org>
2020-10-21 12:14:55 +0200

    footer corrected

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -32,7 +32,7 @@
         <div class="container py-2">
             <div class="row py-4">
                 <div class="col-lg-1 d-flex align-items-center justify-content-center justify-content-lg-start mb-2 mb-lg-0">
-                    <a href="index.html" class="logo pr-0 pr-lg-3">
+                    <a href="/" class="logo pr-0 pr-lg-3">
                         <img alt="Porto Website Template" src="img/logo-klein.png" height="33">
                     </a>
                 </div>
