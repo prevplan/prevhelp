082dbf1a4b17febebb14404ec666ca1120822aa1
Holger Schmermbeck <hosch@mailbox.org>
2020-12-11 17:28:20 +0100

    create error.blade.php

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -34,17 +34,7 @@
         <div class="row">
             <div class="col">
 
-                @if ($errors->any())
-                    <div class="alert alert-danger alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <strong><i class="fas fa-exclamation-triangle"></i>Oh snap!</strong> Change a few things up and try submitting again.
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+                @include('layouts.error')
 
                 <form action="{{ route('event.store', ['course' => $course]) }}" id="form"
                       method="post"
