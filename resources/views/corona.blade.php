25dad8f77bd1958e5fa568e9a580ff9f9714dbe7
Holger Schmermbeck <hosch@mailbox.org>
2021-01-08 09:57:03 +0100

    corona update january

--- resources/views/corona.blade.php
+++ resources/views/corona.blade.php
@@ -29,6 +29,28 @@
                     <article class="post post-large blog-single-post border-0 m-0 p-0">
 
                         <div class="post-date ml-0">
+                            <span class="day">07</span>
+                            <span class="month">Jan</span>
+                        </div>
+
+                        <div class="post-content ml-0">
+
+                            <h2 class="font-weight-bold">Verlängerung des bundesweiten Lockdowns bis einschließlich 31.01.2021</h2>
+
+                            <p>Wie Sie bereits den Medien entnommen haben dürften, wird der aktuelle Lockdown bis zum einschließlich 31.01.2021 verlängert und teilweise sogar verschärft.</p>
+                            <p>Wir hatten bereits direkt nach der Pressekonferenz die Anmeldung für die Kurse im Januar präventiv deaktiviert, zuvor erfolgte Anmeldungen jedoch noch nicht storniert. Wir hatten daher diverse Nachfragen von Teilnehmern, ob diese Kurse nun stattfinden würden oder nicht. Insbesondere da andere Anbieter ihre Kursanmeldungen für Januar weiter offen hatten oder auch jetzt noch offen haben. Hier möchten wir uns bei Ihnen entschuldigen, dass dies zu Verwirrungen geführt haben könnte.</p>
+                            <p>Die Gründe hierfür sind jedoch, dass wir bereits aufgrund der Pressekonferenz davon ausgingen, dass der Schulbetrieb im Januar weiter untersagt werden würde und es keine Ausnahmeregelung geben wird. Daher wollten wir mit der Möglichkeit der Anmeldung für Kurse im Januar keine Hoffnungen schüren, die nun doch enttäuscht werden würden, sondern die verbindliche Verordnung des Landes NRW abwarten.</p>
+                            <p>Diese Verordnung ist zwischenzeitlich erschienen und kann auf der <a href="https://www.mags.nrw/sites/default/files/asset/document/210107_coronaschvo_ab_11.01.2021.pdf">Seite der Landesregierung</a> als .pdf Datei eingesehen werden. Hierin heißt es unter § 7 Abs. 1: <em>Sämtliche Bildungs-, Aus- und Weiterbildungsangebote [...] von [...] sonstigen nicht unter § 6 fallenden öffentlichen, kirchlichen oder privaten außerschulischen Anbietern, Einrichtungen und Organisationen sowie Angebote der Selbsthilfe und musikalischer Unterricht sind in Präsenz untersagt.</em></p>
+                            <p>Dies bedeutet, dass alle Kurse, die <em>in Präsenz</em> stattfinden, bis mind. Ende Januar untersagt sind, unabhängig davon, welche Organisation oder Firma diese anbietet. In diesem Zusammenhang möchten wir klarstellen, dass Angebote von <em>Online-Erste-Hilfe-Kursen</em> von den Berufsgenossenschaften und Behörden <strong>nicht anerkannt</strong> werden! Leider gibt es hier immer wieder <em>schwarze Scharfe</em>, welche auf Ihren Webseiten andere Dinge versprechen. Dies ist de facto falsch. Bitte sparen Sie sich das Geld für solche Kurse, wenn Sie die Bescheinigung für den Betrieb, den Führerschein oder Ähnliches brauchen. Wir werden daher auch in absehbarer Zeit keine Online-Schulungen anbieten, welche ohnehin nicht anerkannt werden würden.</p>
+                            <p>Zusammengefasst bleibt, dass bis vorerst 31.01.2021 <strong>keine</strong> Erste-Hilfe-Kurse in Nordrhein-Westfalen und den meisten anderen Bundesländern stattfinden dürfen. Auch <em>Einzelkurse</em> o.ä. wie diese teilweise bei uns nachgefragt wurden, sind <strong>nicht zulässig</strong>. Die Kurse ab Februar 2021 haben wir selbstverständlich bereits zur Buchung freigegeben. Bei unseren <a href="{{ route('event.overview') }}">Terminen</a> finden Sie die entsprechende Übersicht inkl. der Möglichkeit der Online-Buchung.</p>
+                            <p>Mit aktuellem Stand gehen wir davon aus und hoffen darauf, dass ab Februar wieder Kurse möglich sein werden. Sollte dies nicht der Fall sein und die Regierung den Lockdown weiter verlängern müssen, würden wir Sie selbstverständlich auch hierüber informieren, sobald uns belastbare Informationen vorliegen.</p>
+                            <div align="center"><p><strong>Bitte bleiben Sie gesund!</strong></p></div>
+                        </div>
+                    </article>
+
+                    <article class="post post-large blog-single-post border-0 m-0 p-0">
+
+                        <div class="post-date ml-0">
                             <span class="day">14</span>
                             <span class="month">Dez</span>
                         </div>
